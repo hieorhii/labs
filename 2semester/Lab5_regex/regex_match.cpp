8336c62a66cb6cc59816a59e29389503c0fb2111
Georgy Boev <boevgo@gmail.com>
2024-06-28 13:30:23 +0300

    labs

---
+++ 2semester/Lab5_regex/regex_match.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+#include <regex>
+
+using namespace std;
+
+bool isIPAddress(const string& str) {
+    regex ip_regex("^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$");
+    return regex_match(str, ip_regex);
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    ifstream inputFile("input.txt");
+    ofstream outputFile("output.txt");
+
+    if (!inputFile.is_open()) {
+        cerr << "Ошибка открытия файла input.txt" << endl;
+        return 1;
+    }
+
+    if (!outputFile.is_open()) {
+        cerr << "Ошибка открытия файла output.txt" << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(inputFile, line)) {
+        if (isIPAddress(line)) {
+            outputFile << line << " - Правильный адрес" << endl;
+        }
+        else {
+            outputFile << line << " - Неправильный адрес" << endl;
+        }
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    return 0;
+}
